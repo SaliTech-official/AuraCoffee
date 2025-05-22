cab50f81f431ebdeceab81a37bc86885064cbc1c
Salar <SaliTech.2008@gmail.com>
2025-05-22 21:55:34 +0330

    first commit

---
+++ app/src/App.jsx
@@ -0,0 +1,11 @@
+import Header from './components/Header'
+
+function App() {
+  return (
+    <div className="bg-cream">
+      <Header />
+    </div>
+  )
+}
+
+export default App
---
+++ app/src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <div className='w-full h-[100px] bg-brown text-cream flex items-center justify-center'>
+        <h1 className='text-2xl font-bold font-heading'>Aura Coffee</h1>
+    </div>
+  )
+}
