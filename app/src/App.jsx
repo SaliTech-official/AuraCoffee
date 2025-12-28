f7b74c297c4765b99f209a2ab68fb6d169cedb89
Salar <SaliTech.2008@gmail.com>
2025-12-28 16:36:29 +0330

    Creators Signatures Add

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -2,7 +2,8 @@ import { useState } from 'react'
 import Header from './components/Header'
 import Filter from './components/Filter'
 import Products from './components/Products'
-
+import { GreenEmeraldSignature } from '@salitech-official/green-emerald-signature';
+import '@salitech-official/green-emerald-signature/dist/style.css';
 function App() {
   const [activeFilter, setActiveFilter] = useState("Hot Drinks")
 
@@ -15,6 +16,7 @@ function App() {
       <Header />
       <Filter onFilterChange={handleFilterChange} />
       <Products activeFilter={activeFilter} />
+      <GreenEmeraldSignature lang='en'/>
     </div>
   )
 }
