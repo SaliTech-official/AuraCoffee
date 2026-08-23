8402783d83f8d9acf3c930a6f059e8d28227dfd3
Salar <SaliTech.2008@gmail.com>
2026-08-24 01:01:11 +0400

    CreditBar SaliTech Add

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,24 +1,24 @@
-import { useState } from 'react'
-import Header from './components/Header'
-import Filter from './components/Filter'
-import Products from './components/Products'
-import { GreenEmeraldSignature } from '@salitech-official/green-emerald-signature';
-import '@salitech-official/green-emerald-signature/dist/style.css';
+import { useState } from "react";
+import Header from "./components/Header";
+import Filter from "./components/Filter";
+import Products from "./components/Products";
+import { CreditBar } from "./components/CreditBar";
+
 function App() {
-  const [activeFilter, setActiveFilter] = useState("Hot Drinks")
+  const [activeFilter, setActiveFilter] = useState("Hot Drinks");
 
   const handleFilterChange = (filterTitle) => {
-    setActiveFilter(filterTitle)
-  }
+    setActiveFilter(filterTitle);
+  };
 
   return (
     <div>
       <Header />
       <Filter onFilterChange={handleFilterChange} />
       <Products activeFilter={activeFilter} />
-      <GreenEmeraldSignature lang='en' link='https://instagram.com/greenemerald.studio/'/>
+      <CreditBar />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
